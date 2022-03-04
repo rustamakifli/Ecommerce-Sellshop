bcc6718659f5612acab3e47888c161560737ada8
RustamAkifli <rustamakifli2003@icloud.com>
2022-03-04 20:35:32 +0400

    database_models_rustam

--- django_project/settings.py
+++ django_project/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sellshop',
+        'USER': 'user',
+        'PASSWORD': '12345',
+        'PORT': 5432,
+        'HOST': 'localhost',
     }
 }
 
