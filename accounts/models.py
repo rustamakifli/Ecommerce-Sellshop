38faa8c21dc641f7528f405e3627b14d50e41bc2
RustamAkifli <rustamakifli2003@icloud.com>
2022-04-08 19:20:44 +0400

    accounts deleted

--- accounts/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class User(AbstractUser):
-    bio = models.TextField(max_length=500, blank=True)
-    image = models.ImageField(upload_to='profile_images')
--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
+from django.utils.translation import gettext, gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 from user.models import BillingAddress
-# Register your models here.
+
+
+# admin.site.unregister(BaseUserAdmin)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'bio', 'image')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+admin.site.register(User, UserAdmin)
+
 
 @admin.register(BillingAddress)
 class BillingAddressAdmin(admin.ModelAdmin):
