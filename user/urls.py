79ba1e8dae2cdc2bef9fa9d9e586214eb63e4a8a
elnurnurulu <elnurnurulu@gmail.com>
2022-04-26 19:33:26 +0400

    change password is ok)

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,11 @@
 
 from django.urls import path
-from user.views import account, login_register,logout
+from user.views import account, login_register,logout, ChangePasswordView
 
 urlpatterns = [
     path('account/', account, name='account'),
     path('register/', login_register, name='register'),
     path('login/', login_register, name='login'),
     path('logout/', logout, name='logout'),
-
+    path('password-change/', ChangePasswordView.as_view(), name='password_change'),
 ]
