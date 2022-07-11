c837316a378f6fba34638de7aeb5cf01d542622b
elnurnurulu <elnurnurulu@gmail.com>
2022-07-12 03:09:09 +0400

    form testing

--- sellshop/core/test_app/test_models.py
+++ sellshop/core/test_app/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from core.models import Contact
+from core.models import Contact, Subscriber
 
 class ContactTest(TestCase):
     
@@ -16,6 +16,25 @@ class ContactTest(TestCase):
     def test_str_method (self):
         self.assertEqual(str(self.contact), self.data["name"])
 
-    def tearDown(self): #the test runner will invoke that method after each test and tearDown deletes contact object.
+    def tearDown(self):
         del self.contact
 
+
+class SubscriberTest(TestCase):
+
+    @classmethod
+    def setUp(cls):
+        cls.data = {
+            "email" : "Test@email.com"
+        }
+        cls.subscriber = Subscriber.objects.create(**cls.data)
+
+    def test_model_data(self):
+        self.assertEqual(self.data["email"], self.subscriber.email)
+
+    def test_str_method (self):
+        self.assertEqual(str(self.subscriber), self.data["email"])
+
+    @classmethod
+    def tearDown(cls): #the test runner will invoke that method after each test and tearDown deletes contact object.
+        del cls.subscriber
